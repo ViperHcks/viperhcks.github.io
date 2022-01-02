4bb67640725cdfcd5df2714944e02e72cdd38beb
Rivu <54476266+ViperHcks@users.noreply.github.com>
2022-01-02 19:38:46 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,11 +10,9 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
-
 }
 
 window.onscroll = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
-
 }
